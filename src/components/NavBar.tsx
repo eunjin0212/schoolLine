779ec05e1c50d98a81f58146d09bb28be4708c8b
EunJin Kim <eunjin9639@gmail.com>
2024-09-16 21:29:12 +0900

    ✨ Add NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,18 @@ import { Link, useLocation } from "react-router-dom";
 import { routes } from '../main';
 import { Logo_1008, Logo_1920, Logo_375, Logo_640 } from '../assets/Logo';
 import { Oreo_24, Oreo_28 } from '../assets/Oreo';
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 
 const NavBar = () => {
   const linkPadding = 'lg:py-[1.625rem] py-[0.625rem]'
-  const { pathname } = useLocation();
   const filteredRoutes = routes.filter(route => route.title)
+  const { pathname } = useLocation();
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    setOpen(false)
+  }, [pathname])
 
   return (
     <nav className='flex justify-center h-12 px-6 lg:h-24 md:h-14 lg:px-9 md:px-7'>
@@ -23,19 +30,31 @@ const NavBar = () => {
         {filteredRoutes.map((route) => (
           <li key={route.path} className={[
             linkPadding,
-            'lg:inline-flex lg:items-center lg:justify-center hidden min-w-[6.25rem] text-nowrap',
+            'hidden text-nowrap text-base leading-5 font-medium lg:inline-flex lg:items-center lg:justify-center min-w-[6.25rem]',
             pathname === route.path && 'transition-all relative before:w-full before:h-1 before:bg-green-02 before:absolute before:bottom-0'
           ].join(' ')}>
             <Link to={route.path}>{route.title}</Link>
           </li>
         ))}
         <li className='hidden max-lg:list-item'>
-          <button className='absolute top-0 right-0 inline-flex items-center justify-center w-12 h-12 md:w-14 md:h-14'>
+          <button onClick={() => setOpen((prev) => !prev)} className='absolute top-0 right-0 inline-flex items-center justify-center w-12 h-12 md:w-14 md:h-14'>
             <Oreo_28 className='hidden md:block' />
             <Oreo_24 className='block md:hidden' />
           </button>
         </li>
       </ul>
+      {
+        open &&
+        createPortal(
+          <ul className='absolute w-full bg-white/[.96] md:top-14 top-12 pt-[3px] md:pt-1'>
+            {filteredRoutes.map((route) =>
+              <li key={route.path} className='md:text-lg md:leading-5 leading-[18px] text-sm md:py-4 py-3 md:px-6 px-5 border-b-[0.4px] border-b-neutral-300 last-of-type:border-b-0'>
+                <Link to={route.path} className='inline-flex items-center h-8'>{route.title}</Link>
+              </li>
+            )}
+          </ul>, document.body
+        )
+      }
     </nav>
   )
 }
