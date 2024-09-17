c259fe7df2754f0d16c3be17258ac3aa79b0cb5d
EunJin Kim <eunjin9639@gmail.com>
2024-09-18 00:56:07 +0900

    🍱 Edit svg component attrs

---
+++ src/assets/SecurityTitle.tsx
@@ -0,0 +1,13 @@
+export const SecurityTitle_375 = ({ ...props }) => (
+  <svg {...props} width="21" height="24" viewBox="0 0 21 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10.5 0L0 4.77648V11.3822C0 14.9205 1.77428 18.2231 4.7246 20.1763L10.5 24L16.2754 20.1763C19.2257 18.2231 21 14.9205 21 11.3822V4.77648L10.5 0Z" fill="#A051D4" />
+    <path d="M6.4165 10.8683L9.72731 14.7364L14.5832 8.45068" stroke="white" strokeWidth="2.34375" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
+
+export const SecurityTitle_640 = ({ ...props }) => (
+  <svg {...props} width="36" height="43" viewBox="0 0 36 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18 0.219727L0 8.57858V20.4943C0 26.4723 2.96787 32.0602 7.92076 35.4076L18 42.2197L28.0792 35.4076C33.0321 32.0602 36 26.4723 36 20.4943V8.57858L18 0.219727Z" fill="#A051D4" />
+    <path d="M11 19.2391L16.6757 26.0083L25 15.0083" stroke="white" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
---
+++ src/assets/StudyPlanningSupport.tsx
@@ -0,0 +1,13 @@
+export const StudyPlanningSupport_375 = ({ ...props }) => (
+  <svg {...props} width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="22" height="22" rx="11" fill="#8A3FBB" />
+    <path d="M11.0005 5.79004L11.0007 11.549L13.8952 13.8953" stroke="white" strokeWidth="2.34375" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
+
+export const StudyPlanningSupport_640 = ({ ...props }) => (
+  <svg {...props} width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="38" height="38" rx="19" fill="#8A3FBB" />
+    <path d="M19 10L19.0003 19.9474L24 24" stroke="white" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
