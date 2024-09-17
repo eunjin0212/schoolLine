11ef8d5c7c766bf72db076b302332d6c09d4b3e9
EunJin Kim <eunjin9639@gmail.com>
2024-09-17 16:47:54 +0900

    🍱 Add home assets

---
+++ src/assets/RightArrow.tsx
@@ -0,0 +1,17 @@
+export const RightArrow_16 = ({ ...props }) => (
+  <svg {...props} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3.33105 7.91268H13.1034M13.1034 7.91268L9.54983 4.24805M13.1034 7.91268L9.54983 11.5773" stroke="white" strokeWidth="1.22154" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
+
+export const RightArrow_26 = ({ ...props }) => (
+  <svg {...props} width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4.72559 12.561H21.3779M21.3779 12.561L15.3225 6.31641M21.3779 12.561L15.3225 18.8056" stroke="white" strokeWidth="2.08153" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
+
+export const RightArrow_28 = ({ ...props }) => (
+  <svg {...props} width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4.66602 14.5H23.3327M23.3327 14.5L16.5448 7.5M23.3327 14.5L16.5448 21.5" stroke="white" strokeWidth="2.33333" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+) 
