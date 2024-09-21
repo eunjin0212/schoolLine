235583a98341c01e5ee7eb3103052af3c305e3a4
EunJin Kim <eunjin9639@gmail.com>
2024-09-21 16:31:44 +0900

    🐛 3. 導入の流れ와 お問い合わせ페이지가 바뀌어 있음

--- src/pages/Business.tsx
+++ src/pages/Business.tsx
@@ -1,89 +1,169 @@
-import { Fragment } from 'react/jsx-runtime'
-import { ProgressArrow_1008, ProgressArrow_375, ProgressArrow_640 } from '../assets/ProgressArrow'
-import ContactCard from '../components/ContactCard'
+import { useState, type FormEvent } from 'react'
 import PageHeaderSection from '../components/PageHeaderSection'
-import ProcessCard from '../components/ProcessCard'
+import Field, { type FieldProps } from '../components/Field'
 
-const Business = () => {
-  const processes = [
+const Content = () => {
+  const [formValue, setFormValue] = useState({
+    companyName: '',
+    lastName: '',
+    firstName: '',
+    canaLastName: '',
+    canaFirstName: '',
+    email: '',
+    phoneNumber: '',
+    inquiryDetail: '',
+  })
+
+  const fields = [
     {
-      title: 'お問い合わせページから<br class="block md:hidden" />フォー<br class="hidden md:block lg:hidden" />ムを作成',
-      description: 'お問い合わせページでフォームにご記入の上、ご提出いただければ担当者から速やかにご連絡いたします。',
+      label: '会社名・所属先名',
+      name: 'companyName' as const,
+      type: 'text' as FieldProps['type'],
+      required: true
+    },
+    [
+      {
+        label: '姓',
+        name: 'lastName' as const,
+        type: 'text' as FieldProps['type'],
+        required: true
+      },
+      {
+        label: '名',
+        name: 'firstName' as const,
+        type: 'text' as FieldProps['type'],
+        required: true
+      },
+    ],
+    [
+      {
+        label: '姓（カナ）',
+        name: 'canaLastName' as const,
+        type: 'text' as FieldProps['type'],
+        required: true
+      },
+      {
+        label: '名（カナ）',
+        name: 'canaFirstName' as const,
+        type: 'text' as FieldProps['type'],
+        required: true
+      }
+      ,
+    ],
+    {
+      label: 'メールアドレス',
+      name: 'email' as const,
+      type: 'email' as FieldProps['type'],
+      required: true
     },
     {
-      title: '担当者の連絡',
-      description: 'フォームにご記入いただいたお電話、またはメールでSCHOOL Lineをご説明いたします。<br class="hidden xl:block" /> お問い合わせにご記入いただいた内容とご不明な点など、どれでもお知らせします。',
+      label: '電話番号',
+      name: 'phoneNumber' as const,
+      type: 'tel' as FieldProps['type'],
+      required: true
     },
     {
-      title: 'サービス利用申請',
-      description: '相談を通じてすぐにサービスの使用をご希望の場合は、担当者が受付いたします。<br class="hidden xl:block" />作成していただいたフォームに基づいて、新しい情報の入力が必要になる場合があります。',
+      label: 'お問い合わせ内容',
+      name: 'inquiryDetail' as const,
+      type: 'textarea' as FieldProps['type'],
+      required: false
     },
-    {
-      title: 'アカウント作成',
-      description: '申し込み受付後、契約が締結されたらSCHOOL Lineがアカウントを準備いたします。',
-    },
-    {
-      title: '初期設定完了後に使用',
-      description: '案内に従って、学生、先生、両親、教職員の名簿を登録した後、初期設定が完了したら、配布<br class="hidden xl:block" />したアカウントを通じてサービスを利用できます。',
-    },
-  ]
-  return (
-    <main>
-      <PageHeaderSection title='導入の流れ'>
-        <>
-          <h2 className={`
-            mb-[18px] text-lg leading-5 pt-7
-            md:mb-[14px] md:text-[28px] md:leading-9 md:pt-[76px]
-            lg:mb-7 lg:leading-[34px] lg:pt-[68px]
+  ];
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+  const handleInputChange = (value: string, name: keyof typeof formValue) => {
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  }
+
+  return (<main>
+    <PageHeaderSection title='お問い合わせ'>
+      <>
+        <h2 className={`
+            mb-[18px] text-base leading-5 pt-[37px]
+            md:mb-7 md:text-[28px] md:leading-[34px] md:pt-[68px]
             font-bold text-center
           `}>
-            SCHOOL Line 導入される過程
-          </h2>
-          <p className={`
-            text-xs leading-[19px] pb-7
-            md:w-[468px] md:text-[18px] md:leading-8 md:pb-[75px]
-            lg:w-full lg:pb-[69px] lg:leading-5
-            text-center -tracking-[0.04em]
+          SCHOOL Line 利用 / 質問お問い合わせ
+        </h2>
+        <p className={`
+            w-[260px] text-xs leading-[18px] pb-5
+            md:w-[442px] md:text-[18px] md:leading-[30px] md:pb-[32px]
+            lg:pb-[30px]
+            xl:w-full xl:pb-[68px]
+            text-center -tracking-[0.04em] text-gray-08 font-medium
           `}>
-            学校側でSCHOOL Lineを導入する過程を<br className='block md:hidden' />
-            お知らせします
-          </p>
-        </>
-      </PageHeaderSection>
-      <section className='mt-12 md:mt-[99px]'>
-        <div className={`
-          w-[343px] p-6 bg-gray-01 rounded-3xl gap-2
-          md:w-[584px] md:px-[42px] md:py-14 md:rounded-[44px]
-          lg:w-[784px] lg:p-14 lg:gap-[18px]
-          xl:w-[1360px] xl:p-[82px]
-          mx-auto flex flex-col items-center
-        `}>
-          {processes.map((process, idx) =>
-            <Fragment key={idx}>
-              <ProcessCard idx={idx} {...process} />
-              {
-                idx < processes.length - 1 &&
-                <>
-                  <ProgressArrow_375 className='block md:hidden' />
-                  <ProgressArrow_640 className='hidden md:block lg:hidden' />
-                  <ProgressArrow_1008 className='hidden lg:block' />
-                </>
-              }
-            </Fragment>
-          )}
+          スクールラインを通じて先生と学生、両親とのコミュニケーションを円滑にすることができます
+        </p>
+      </>
+    </PageHeaderSection>
+    <section>
+      <form
+        onSubmit={handleSubmit}
+        className={`
+          w-[343px] pt-11 pb-12
+          md:w-[584px] md:pt-[98px] md:pb-[176px]
+          lg:w-[784px] lg:pt-[100px] lg:pb-[200px]
+          xl:w-[1200px] xl:pt-[127px]
+          flex flex-col mx-auto`
+        }>
+        <div className='gap-[14px] md:gap-10 flex flex-col'>
+          {fields.map((field, idx) =>
+            Array.isArray(field)
+              ? (
+                <div className='gap-[15px] md:gap-4 grid grid-cols-2' key={`field_${idx}`}>
+                  {field.map((item) =>
+                    <Field
+                      {...item}
+                      key={item.name}
+                      value={formValue[item.name]}
+                      onChange={(val) => handleInputChange(val, item.name)}
+                    />
+                  )}
+                </div>
+              )
+              : (
+                <Field
+                  {...field}
+                  key={field.name}
+                  value={formValue[field.name]}
+                  onChange={(val) => handleInputChange(val, field.name)}
+                />
+              ))}
         </div>
-      </section>
-      <section className='pb-12 md:pb-[100px] lg:pb-[200px]'>
-        <div className='w-[343px] md:w-[584px] lg:w-[784px] xl:w-[1360px] mx-auto'>
-          <ContactCard
-            title='お問い合わせからすぐに使ってみてください'
-            description='スクールラインを通じて先生と学生'
-            button='お問い合わせ'
-          />
+        <div className='mx-auto mb-8 mt-11 md:mt-20 md:mb-12'>
+          <strong className='text-xs md:text-sm leading-[14px] md:leading-4 mb-[9px] md:mb-4'>お問い合わせ前にご確認ください</strong>
+          <p className='text-xs md:text-sm leading-[18px] md:leading-5 lg:leading-6'>お問い合わせの内容によっては、お返事にお時間をいただく場合がございます。あらかじめご了承くださいますようお願いいたします。ご入力メールアドレスに誤りがある場合や受信設定されていない場合は、返信できかねます。お手数ですが、送信前に今一度ご確認をお願いします。</p>
         </div>
-      </section>
-    </main>
-  )
+        <div className='py-[18px] px-5 md:p-8 rounded-[7px] md:rounded-xl mb-9 md:mb-16 mx-auto xl:mx-0 bg-purple-05'>
+          <strong className='text-xs md:text-sm leading-[14px] md:leading-4 mb-[9px] md:mb-4'>個人情報の取り扱いについて</strong>
+          <div className='text-xs md:text-sm leading-[18px] md:leading-5 lg:leading-6'>
+            当社は、個人情報保護に関する法律及びその他の関連法令等を遵守し、管理責任者の下で厳重なセキュリティ対策を実施し、適正に個人情報を管理しています。<br />
+            当社は、お客様から取得した個人情報を次の目的で利用します。
+            <ul>
+              <li>
+                - 当社サービスの提供やお客様へのお問い合わせ対応などのサポートを提供
+              </li>
+              <li>
+                - 当社または株式会社Knowlegeshare、株式会社UC Wareの商品/サービス/キャンペーンについてのご案内
+              </li>
+              <li>
+                - 各種調査、統計·マーケティング資料の作成および研究、企画および開発
+              </li>
+              <li>
+                - その他当社プライバシーポリシーで示す目的
+              </li>
+            </ul>
+          </div>
+        </div>
+        <button className='h-9 md:h-[52px] w-[210px] md:w-[360px] rounded-[30px] md:rounded-[52px] text-xs md:text-xl leading-[14px] md:leading-6 mx-auto font-bold text-white bg-purple-01'>個人情報の取扱いに同意して提出</button>
+      </form>
+    </section>
+  </main>)
 }
 
-export default Business
+export default Content
