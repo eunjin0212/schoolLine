a0af94d554aec354ee182e1c4d4b64fe0ac35b9d
EunJin Kim <eunjin9639@gmail.com>
2024-09-21 18:40:08 +0900

    🍱 오레오 아이콘 tsx에 맞춰 attr 변경

--- src/assets/Oreo.tsx
+++ src/assets/Oreo.tsx
@@ -1,15 +1,15 @@
 export const Oreo_640 = ({ ...props }) => (
   <svg {...props} width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 4H26" stroke="#272727" stroke-width="3" stroke-linecap="round" />
-    <path d="M2 14H26" stroke="#272727" stroke-width="3" stroke-linecap="round" />
-    <path d="M2 24H26" stroke="#272727" stroke-width="3" stroke-linecap="round" />
+    <path d="M2 4H26" stroke="#272727" strokeWidth="3" strokeLinecap="round" />
+    <path d="M2 14H26" stroke="#272727" strokeWidth="3" strokeLinecap="round" />
+    <path d="M2 24H26" stroke="#272727" strokeWidth="3" strokeLinecap="round" />
   </svg>
 )
 
 export const Oreo_375 = ({ ...props }) => (
   <svg {...props} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M1.71436 3.42969H22.2858" stroke="#272727" stroke-width="2.57143" stroke-linecap="round" />
-    <path d="M1.71436 12.002H22.2858" stroke="#272727" stroke-width="2.57143" stroke-linecap="round" />
-    <path d="M1.71436 20.5723H22.2858" stroke="#272727" stroke-width="2.57143" stroke-linecap="round" />
+    <path d="M1.71436 3.42969H22.2858" stroke="#272727" strokeWidth="2.57143" strokeLinecap="round" />
+    <path d="M1.71436 12.002H22.2858" stroke="#272727" strokeWidth="2.57143" strokeLinecap="round" />
+    <path d="M1.71436 20.5723H22.2858" stroke="#272727" strokeWidth="2.57143" strokeLinecap="round" />
   </svg>
 )
